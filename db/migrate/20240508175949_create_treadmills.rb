4c2b4de931bc6e8b455895fe8c142f83df32ed7b
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:39:39 -0400

    Update 20240508175949_create_treadmills.rb

--- db/migrate/20240508175949_create_treadmills.rb
+++ db/migrate/20240508175949_create_treadmills.rb
@@ -1,9 +1,13 @@
 class CreateTreadmills < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'treadmills'
     create_table :treadmills do |t|
+      # This adds an integer column 'value' to the 'treadmills' table
       t.integer :value
+      # This adds a reference column 'user_id' to associate each treadmill entry with a user
       t.references :user, null: false, foreign_key: true
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'treadmills' table
       t.timestamps
     end
   end
