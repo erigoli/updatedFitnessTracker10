c4d0b0e47d6242ed64aa84d39a882cb3fd1e53e9
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:34:39 -0400

    Update 20240508175533_create_steps.rb

--- db/migrate/20240508175533_create_steps.rb
+++ db/migrate/20240508175533_create_steps.rb
@@ -1,9 +1,13 @@
 class CreateSteps < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'steps'
     create_table :steps do |t|
+      # This adds an integer column 'value' to the 'steps' table
       t.integer :value
+      # This adds a foreign key 'user_id' referencing the 'users' table, ensuring it's not null
       t.references :user, null: false, foreign_key: true
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'steps' table
       t.timestamps
     end
   end
