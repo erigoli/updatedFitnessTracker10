23aabcd745d8419b3d90202166e4597cf4d578d4
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:40:02 -0400

    Update 20240508180007_create_strengths.rb

--- db/migrate/20240508180007_create_strengths.rb
+++ db/migrate/20240508180007_create_strengths.rb
@@ -1,9 +1,13 @@
 class CreateStrengths < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'strengths'
     create_table :strengths do |t|
+      # This adds an integer column 'value' to the 'strengths' table
       t.integer :value
+      # This adds a reference column 'user_id' to associate each strength entry with a user
       t.references :user, null: false, foreign_key: true
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'strengths' table
       t.timestamps
     end
   end
