ada5588cfccebcea738c3e2d4e144e8754c65daa
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:46:12 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
+  # Route to the 'past_inputs#index' action
   get 'past_inputs/index'
-# config/routes.rb
+
+  # Define root route to 'sessions#login'
   root 'sessions#login'
+
+  # Route to 'pages#index' with aliases '/home' and '/pages'
   get '/home', to: 'pages#index', as: 'home' 
   get '/pages', to: 'pages#index'
   
+  # Redirect routes for easy, medium, and hard paths
   get '/easy', to: redirect('/ayeshahtmls/easy.html')
   get '/medium', to: redirect('/ayeshahtmls/medium.html')
   get '/hard', to: redirect('/ayeshahtmls/hard.html')
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health check route to verify application status
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Resources routes for various data models
   resources :cardios, only: [:new, :create]
   resources :steps, only: [:new, :create]
   resources :water_intakes, only: [:new, :create]
@@ -33,8 +35,13 @@ Rails.application.routes.draw do
   resources :weightlosses, only: [:new, :create]
   resources :weightlossgoals, only: [:new, :create]
 
+  # Explicit route for creating a new calorie entry
   get '/calories/new', to: 'calories#new', as: 'new_calorie'
+
+  # Resources route for users with limited actions
   resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
+  # Routes for user login/logout
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
