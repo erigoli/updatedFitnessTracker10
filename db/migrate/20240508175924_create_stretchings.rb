64002466fcf5fe96d513ccc62fcc1e49fa2fcb0b
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:38:38 -0400

    Update 20240508175924_create_stretchings.rb

--- db/migrate/20240508175924_create_stretchings.rb
+++ db/migrate/20240508175924_create_stretchings.rb
@@ -1,9 +1,13 @@
 class CreateStretchings < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'stretchings'
     create_table :stretchings do |t|
+      # This adds an integer column 'value' to the 'stretchings' table
       t.integer :value
+      # This adds a reference column 'user_id' to associate each stretching entry with a user
       t.references :user, null: false, foreign_key: true
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'stretchings' table
       t.timestamps
     end
   end
