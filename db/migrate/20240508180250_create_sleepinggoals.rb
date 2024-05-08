cd7fb3d5943abb1729e0050dd95f7d0976b3dda8
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:42:55 -0400

    Update 20240508180250_create_sleepinggoals.rb

--- db/migrate/20240508180250_create_sleepinggoals.rb
+++ db/migrate/20240508180250_create_sleepinggoals.rb
@@ -1,9 +1,13 @@
 class CreateSleepinggoals < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'sleepinggoals'
     create_table :sleepinggoals do |t|
+      # This adds an integer column 'value' to the 'sleepinggoals' table
       t.integer :value
+      # This adds a reference column 'user_id' to associate each sleeping goal with a user
       t.references :user, null: false, foreign_key: true
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'sleepinggoals' table
       t.timestamps
     end
   end
