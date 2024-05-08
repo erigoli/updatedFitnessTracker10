d0e15da4269759892ebfe6146af896b437d7b459
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:43:40 -0400

    Update 20240508180333_create_weightlossgoals.rb

--- db/migrate/20240508180333_create_weightlossgoals.rb
+++ db/migrate/20240508180333_create_weightlossgoals.rb
@@ -1,9 +1,13 @@
 class CreateWeightlossgoals < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'weightlossgoals'
     create_table :weightlossgoals do |t|
+      # This adds an integer column 'value' to the 'weightlossgoals' table
       t.integer :value
+      # This adds a reference column 'user_id' to associate each weight loss goal with a user
       t.references :user, null: false, foreign_key: true
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'weightlossgoals' table
       t.timestamps
     end
   end
