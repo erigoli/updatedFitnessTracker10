1157f2ea13201500ba51073d12de7b98ae1aa0c7
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:40:58 -0400

    Update 20240508180056_create_heights.rb

--- db/migrate/20240508180056_create_heights.rb
+++ db/migrate/20240508180056_create_heights.rb
@@ -1,9 +1,13 @@
 class CreateHeights < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'heights'
     create_table :heights do |t|
+      # This adds an integer column 'value' to the 'heights' table
       t.integer :value
+      # This adds a reference column 'user_id' to associate each height entry with a user
       t.references :user, null: false, foreign_key: true
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'heights' table
       t.timestamps
     end
   end
