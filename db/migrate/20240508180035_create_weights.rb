0df90b4abb9c8476c0c185f5ea86b1fc7aabcf6a
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:40:26 -0400

    Update 20240508180035_create_weights.rb

--- db/migrate/20240508180035_create_weights.rb
+++ db/migrate/20240508180035_create_weights.rb
@@ -1,9 +1,13 @@
 class CreateWeights < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'weights'
     create_table :weights do |t|
+      # This adds an integer column 'value' to the 'weights' table
       t.integer :value
+      # This adds a reference column 'user_id' to associate each weight entry with a user
       t.references :user, null: false, foreign_key: true
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'weights' table
       t.timestamps
     end
   end
