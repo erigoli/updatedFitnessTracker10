d2830ee34c41c73bb289f3ffd0dc90e11034c61a
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:41:21 -0400

    Update 20240508180118_create_ages.rb

--- db/migrate/20240508180118_create_ages.rb
+++ db/migrate/20240508180118_create_ages.rb
@@ -1,9 +1,13 @@
 class CreateAges < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'ages'
     create_table :ages do |t|
+      # This adds an integer column 'value' to the 'ages' table
       t.integer :value
+      # This adds a reference column 'user_id' to associate each age entry with a user
       t.references :user, null: false, foreign_key: true
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'ages' table
       t.timestamps
     end
   end
