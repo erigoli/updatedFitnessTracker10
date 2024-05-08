bb11b95507312d535bb0c698a643169e28032831
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:36:36 -0400

    Update 20240508175653_create_water_intakes.rb

--- db/migrate/20240508175653_create_water_intakes.rb
+++ db/migrate/20240508175653_create_water_intakes.rb
@@ -1,9 +1,13 @@
 class CreateWaterIntakes < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'water_intakes'
     create_table :water_intakes do |t|
+      # This adds a decimal column 'gallons' to the 'water_intakes' table
       t.decimal :gallons
+      # This adds a foreign key 'user_id' referencing the 'users' table, ensuring it's not null
       t.references :user, null: false, foreign_key: true
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'water_intakes' table
       t.timestamps
     end
   end
