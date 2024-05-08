0964f8acbe0abaaad9ea4be08a375771c2b76a1d
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:34:17 -0400

    Update 20240507175812_create_users.rb

--- db/migrate/20240507175812_create_users.rb
+++ db/migrate/20240507175812_create_users.rb
@@ -1,9 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'users'
     create_table :users do |t|
+      # This adds a string column 'username' to the 'users' table
       t.string :username
+      # This adds a string column 'password_digest' to the 'users' table
       t.string :password_digest
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'users' table
       t.timestamps
     end
   end
