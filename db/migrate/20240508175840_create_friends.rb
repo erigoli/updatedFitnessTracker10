9c7475aeafa3671839f0848d0257d2586347fe2a
erigoli <156977402+erigoli@users.noreply.github.com>
2024-05-08 19:37:29 -0400

    Update 20240508175840_create_friends.rb

--- db/migrate/20240508175840_create_friends.rb
+++ db/migrate/20240508175840_create_friends.rb
@@ -1,10 +1,13 @@
 class CreateFriends < ActiveRecord::Migration[7.1]
   def change
+    # This method creates a new database table named 'friends'
     create_table :friends do |t|
+      # This adds string columns 'first', 'last', and 'phone' to the 'friends' table
       t.string :first
       t.string :last
       t.string :phone
 
+      # These automatically add 'created_at' and 'updated_at' timestamp columns to the 'friends' table
       t.timestamps
     end
   end
